37d500f57b588b70d02492c2e47b6096555cdc45
Erica Xu <erica.xu@uwaterloo.ca>
2014-10-03 00:27:26 -0700

    Added layout for product detail page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,9 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
+
+    public static Result product() { return ok(product.render()); }
 
 }
