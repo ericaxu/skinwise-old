c8969348a0aafec4ac71395ffa397282c6dbce14
Erica Xu <erica.xu@uwaterloo.ca>
2014-10-25 03:53:56 -0700

    Use z-index the right way TM

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -147,7 +147,6 @@ function initBrowse(type) {
         $('.open_add_filter_popup').on('click', function () {
             $('#add_filter_btn').data('type', $(this).data('type'));
             enableAutocomplete($(this).data('type'), $('#add_filter'), '#add_filter_form .inputs', SW.AUTOCOMPLETE_LIMIT.ADD_FILTER, $('#add_filter_not_found'));
-            $('.filter_area').hide();
             $('.add_filter.popup').show();
         });
 
