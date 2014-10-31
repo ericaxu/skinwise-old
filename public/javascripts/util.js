a10de5b72f5dd11d091edc23372057435f145cab
Erica Xu <erica.xu@uwaterloo.ca>
2014-10-31 00:51:41 -0700

    Fix product image alignment problem.

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -82,7 +82,7 @@ function productResultHTML(product) {
     var $list_item = $('<li/>', {class: 'product'});
     var $link = $('<a/>', {href: '/product/' + product.id});
     var $img = $('<img/>', {
-        class: 'product_pic',
+        class: 'product_img',
         src: product.image || SW_PLACEHOLDER_URL,
         alt: product.brand + ' ' + product.name
     });
@@ -91,7 +91,10 @@ function productResultHTML(product) {
         checkImage($img, product.image);
     }
 
-    $link.append($img);
+    var $img_container = $('<div/>', { class: 'product_img_container' });
+    $img_container.append($img);
+
+    $link.append($img_container);
     $link.append($('<div/>', {class: 'product_brand', text: SW.BRANDS[product.brand].name}));
     $link.append($('<div/>', {class: 'product_name', text: product.name}));
 
