1aded9b769167e49b7a45f6133ed8067a8c7075a
Erica Xu <erica.xu@uwaterloo.ca>
2014-11-03 22:51:31 -0800

    Refactor frontend code.

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,25 +1,3 @@
-function setupProfileAutocomplete() {
-    var profile_fields = [ 'add_ingredient_work', 'add_ingredient_doesnt_work', 'add_ingredient_bad_reaction' ];
-    for (var i = 0; i < profile_fields.length; i++) {
-        var name = profile_fields[i];
-        enableAutocomplete('ingredient',
-            $('#' + name),
-            '#' + name + '_container',
-            SW.AUTOCOMPLETE_LIMIT.ADD_FILTER,
-            $('#' + name + '_not_found'));
-    }
-}
-
-function handleAddIngredientToProfile() {
-    $('#add_ingredient_work_btn').on('click', function() {
-        var name = $('#add_ingredient_work').val();
-        var id = $('#add_ingredient_work').data('id');
-
-        var list = $(this).parent().parent().find('.preference_list');
-        list.append(preferenceListItemHTML(name, id));
-    });
-}
-
 function preferenceListItemHTML(name, id) {
     var $li = $('<li/>');
     $li.append($('<a/>', {
@@ -36,26 +14,26 @@ function preferenceListItemHTML(name, id) {
 
 function loadPreferenceList(list) {
     var $div = $('#' + list.id + '_section');
-    var $list = $('<ul/>', { class: 'preference_list' });
+    var $list = addEl('ul', $div, 'preference_list');
     for (var i = 0; i < list.items.length; i++) {
         var item = list.items[i];
         $list.append(preferenceListItemHTML(item.name, item.id));
     }
-    $div.append($list);
-    var $add_ingredient = $('<div/>', { class: 'field ' + list.id + '_add' });
-    $add_ingredient.append($('<label/>').text('Add another: '));
-    var $add_ingredient_input = $('<input/>');
-    $add_ingredient.append($add_ingredient_input);
-    $div.append($add_ingredient);
-    var $add_ingredient_btn = $('<input/>', { type: 'button', value: 'Add' }).on('click', function() {
+    var $add_ingredient = addEl('div', $div, 'field ' + list.id + '_add');
+    addEl('label', $add_ingredient, '', 'Add another: ');
+    var $add_ingredient_input = addEl('input', $add_ingredient);
+    var $add_ingredient_btn = addEl('input', $add_ingredient, '', '',  { type: 'button', value: 'Add' });
+    $add_ingredient_btn.on('click', function() {
         var name = $add_ingredient_input.val();
         var id = $add_ingredient_input.data('id');
+
+        if (name === '' || id === undefined) {
+            return;
+        }
         $list.append(preferenceListItemHTML(name, id))
         $add_ingredient_input.val('');
     });
-    $div.append($add_ingredient_btn);
-    var $no_result = $('<div/>', { class: 'preference_not_found' }).text('No results found.');
-    $div.append($no_result);
+    var $no_result = addEl('div', $div, 'preference_not_found', 'No results found.');
     enableAutocomplete('ingredient', $add_ingredient_input, '.' + list.id + '_add', SW.AUTOCOMPLETE_LIMIT.ADD_FILTER, $no_result);
 }
 
